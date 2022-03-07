bddce389f541bb3fd671d123ced78e01691f0310
NickRose30 <nicholasrose30@gmail.com>
2022-03-06 22:01:51 -0500

    add amplify

--- src/components/CustomerDrawer.jsx
+++ src/components/CustomerDrawer.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import { Drawer, DrawerSize } from '@blueprintjs/core';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  padding: 16px;
+`;
 
 const CustomerDrawer = ({ isOpen, onClose }) => {
   return (
     <Drawer isOpen={isOpen} size={DrawerSize.SMALL} onClose={onClose}>
-      <div>this is some drawer content</div>
+      <Container>this is some drawer content</Container>
     </Drawer>
   );
 };
